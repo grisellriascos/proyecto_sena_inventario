cb2c6d8980d0f533bebd59285f936f8ea782ec2e
grisellriascos <geriascos21@misena.edu.co>
2022-11-11 00:29:07 +0100

    migrate

---
+++ proyectoInventario-app/database/migrations/2022_11_10_223254_create_personas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres');
+            $table->string('apellidos');
+          
+            $table->integer('edad')->nullable();
+            $table->date('fecha_nacimiento')->nullable();
+            $table->foreignId('cargo_Id')->constrained('cargos');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+};
